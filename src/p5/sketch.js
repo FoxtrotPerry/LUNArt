4388f6387429c7acd04c6e6dad0cde2594dd3757
Caleb Perry <igadabif32@gmail.com>
2018-11-03 17:59:19 -0400

    First useful looking graphic

--- src/p5/sketch.js
+++ src/p5/sketch.js
@@ -1,7 +1,23 @@
+var earthquakes;
+function preload() {
+  // Get the most recent earthquake in the database
+  var url =
+   'https://earthquake.usgs.gov/earthquakes/feed/v1.0/' +
+    'summary/all_day.geojson';
+  earthquakes = loadJSON(url);
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  noLoop();
 }
 
 function draw() {
-  background(220);
+  background(200);
+  // Get the magnitude and name of the earthquake out of the loaded JSON
+  let earthquakeMag = earthquakes.features[0].properties.mag;
+  let earthquakeName = earthquakes.features[0].properties.place;
+  ellipse(width / 2, height / 2, earthquakeMag * 10, earthquakeMag * 10);
+  textAlign(CENTER);
+  text(earthquakeName, 0, height - 30, width, 30);
 }
