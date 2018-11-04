719a6c63d1a904a4890367908f72318fc09a4efa
Gabe <kuslanskyg@gmail.com>
2018-11-03 23:06:33 -0400

    removed console logs

--- src/p5/sketch.js
+++ src/p5/sketch.js
@@ -2,9 +2,7 @@ let solarEclipseData, lunarPhaseData, asteroidsData;
 let tick = 0;
 
 const updateData = async() => {
-  //console.log(tick % 60)
   if (tick % 60 === 0) {
-    console.log('query')
     solarData = await getSolarEclipses();
     lunarPhaseData = await getLunarPhases();
     //asteroidsData = await getAsteroids();
