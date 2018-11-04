2253aa7fd432984ffcc12725312e694ba9a26945
Caleb Perry <igadabif32@gmail.com>
2018-11-04 10:57:45 -0500

    Updating meteors

--- src/dataLogic/drawMeteorShower.js
+++ src/dataLogic/drawMeteorShower.js
@@ -1,5 +1,5 @@
 function drawMeteorShower(meteors) {
-    for(let i = 0; i < 25; i++) {
+    for(let i = 0; i < 40; i++) {
         fill(255,245,245);
         noStroke();
         ellipse(meteors[i][0],meteors[i][1],4);
