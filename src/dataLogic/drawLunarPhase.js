424d9de5e5c8cd4d8a9c49b6654ad177aa4add72
Luke Krentz <lukekrentz@gmail.com>
2018-11-04 05:18:44 -0500

    Getting Phases to work

--- src/dataLogic/drawLunarPhase.js
+++ src/dataLogic/drawLunarPhase.js
@@ -1,34 +1,42 @@
 let phaseMod = 0;
+let eD = 0;
+let tD = 0;
+let dOver = 0;
 
 function drawLunarPhase(data) {
     if(data) {
         const moonPhase = data[0].phase;
         const progress = getTimeProgress();
         noStroke();
-        fill(40);
-        ellipse(windowWidth/2, windowHeight/2, 500);
-        fill(255,245,200);
 
         switch(moonPhase) {
             case "New Moon": (() => {
-                arc(windowWidth/2, windowHeight/2, 500*phaseMod, 500,HALF_PI,HALF_PI+PI);
-                arc(windowWidth/2, windowHeight/2, 500, 500, HALF_PI+PI,HALF_PI);
+                const ratio = calcRatio(data[0].date, data[1].date);
+                fill(40);
+                ellipse(windowWidth/2, windowHeight/2, 500);
+                fill(255,245,200);
+                arc(windowWidth/2, windowHeight/2, 500, 500, HALF_PI, HALF_PI + PI);
+                arc(windowWidth/2, windowHeight/2, (500 - (500 * ratio)), 500, HALF_PI + PI, HALF_PI);
             })();
-            
+            break;
+
             case "Full Moon": (() => {
-                arc(windowWidth/2, windowHeight/2, 500*phaseMod, 500,HALF_PI,HALF_PI+PI);
-                arc(windowWidth/2, windowHeight/2, 500, 500, HALF_PI+PI,HALF_PI);
+                arc(windowWidth/2, windowHeight/2, 500*phaseMod, 500, HALF_PI, HALF_PI + PI);
+                arc(windowWidth/2, windowHeight/2, 500, 500, HALF_PI + PI, HALF_PI);
             })();
+            break;
 
             case "First Quarter": (() => {
-                arc(windowWidth/2, windowHeight/2, 500*phaseMod, 500,HALF_PI,HALF_PI+PI);
-                arc(windowWidth/2, windowHeight/2, 500, 500, HALF_PI+PI,HALF_PI);
+                arc(windowWidth/2, windowHeight/2, 500*phaseMod, 500, HALF_PI, HALF_PI + PI);
+                arc(windowWidth/2, windowHeight/2, 500, 500, HALF_PI + PI, HALF_PI);
             })();
+            break;
 
             case "Last Quarter": (() => {
-                arc(windowWidth/2, windowHeight/2, 500*phaseMod, 500,HALF_PI,HALF_PI+PI);
-                arc(windowWidth/2, windowHeight/2, 500, 500, HALF_PI+PI,HALF_PI);
+                arc(windowWidth/2, windowHeight/2, 500*phaseMod, 500, HALF_PI, HALF_PI + PI);
+                arc(windowWidth/2, windowHeight/2, 500, 500, HALF_PI + PI, HALF_PI);
             })();
+            break;
         }
 
         phaseMod += .001;
@@ -36,6 +44,16 @@ function drawLunarPhase(data) {
           phaseMod=0;
         }
     }
+}
+
+function calcRatio(currentDate, endDate) {
+    let temp = currentDate.split(" ");
+    currentDate = temp[2];
+    temp = endDate.split(" ");
+    endDate = temp[2];
+
+    const ratio = (((endDate - currentDate) + getTimeProgress()) / 7.5);
+    return ratio;
 }
 
 function calculateDayDifferenceFromCurrentDate(date) {
