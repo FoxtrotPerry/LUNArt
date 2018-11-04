d5d3b2257a3cf01354c618a1df9576685985499c
Gabe <kuslanskyg@gmail.com>
2018-11-04 00:42:44 -0400

    Fixed eclipse with sun

--- src/dataLogic/drawSolarEclipse.js
+++ src/dataLogic/drawSolarEclipse.js
@@ -1,16 +1,20 @@
 
+let dayDifference = 60;
+
 function drawSolarEclipse(data) {
     //Eclipse
     if (data) {
+        dayDifference-= 0.25;
         const currentDay = new Date().getDay();
-        const dayDifference = 2;
-        if (dayDifference < 5) {
+    
+        if (Math.abs(dayDifference+1) < 10) {
+            tint(255, 255-(Math.abs(dayDifference)*25));
             image(solarEclipseImg, (windowWidth/2)-450, 50, 0);
         }
         
         //Sun    
     
         fill('yellow');
-        ellipse((windowWidth/2 + dayDifference) * 1.2, windowHeight/2, 510, 510);
+        ellipse((windowWidth/2 + dayDifference), windowHeight/2, 510, 510);
     }
 }
