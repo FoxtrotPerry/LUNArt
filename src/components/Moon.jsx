8e3cdf913e02a22b0e41b6fbdee1c8d47979a09b
Luke Krentz <lukekrentz@gmail.com>
2019-04-13 02:01:59 -0400

    Working React Moon Sketch

--- src/components/Moon.jsx
+++ src/components/Moon.jsx
@@ -4,40 +4,57 @@ import P5Wrapper from "../components/P5Wrapper.jsx"
 
 class Moon extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+
         this.state = {
-
-        }
+            error: null,
+            data: null,
+            isLoaded: false,
+        };
     }
 
-    componentDidMount = () => {
+    componentDidMount() {
+        //Get the current date for Fetch call 
+        const currentYear = new Date().getFullYear();
+        const currentDay = new Date().getDate();
+        const currentMonth = new Date().getMonth()+1;
+        const date = currentMonth + '/' + currentDay + '/' + currentYear;
+
+        fetch(`http://api.usno.navy.mil/moon/phase?date=${date}&nump=2`)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        data: result.phasedata,
+                    });
+                },
+
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+        )
 
     }
-
-    getLunarPhases = () => {
-        const currentYear = new Date().getFullYear()
-        const currentDay = new Date().getDate()
-        const currentMonth = new Date().getMonth()
-        const date = currentMonth + '/' + currentDay + '/' + currentYear
-
-        return new Promise((resolve, reject) => {
-            fetch(`http://api.usno.navy.mil/moon/phase?date=${date}&nump=2`).then(res => {
-                if (!res.data.phasedata) {
-                    resolve(res.data.phasedata)
-                }
-            })
-                .catch(e => reject(e))
-        })
-    }
-
-
 
     render() {
-        
-        return (
-            <P5Wrapper sketch={moonSketch}/>
-        )
+        if (this.state.error) {
+            return(
+                <div>ERROR: {this.state.error.message}</div>
+            );
+        } else if (!this.state.isLoaded) {
+            return(
+                <div>Loading...</div>
+            );
+        } else {
+            return(
+                <P5Wrapper sketch={moonSketch} data={this.state.data} />
+            );
+        }
     }
 }
 
-export default Moon
+export default Moon;
