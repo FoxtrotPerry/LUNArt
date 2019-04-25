c5651c48edd07b0c7f0029aaefa4a54d98d24d08
Luke Krentz <lukekrentz@gmail.com>
2019-04-25 18:47:51 -0400

    Update Moon.jsx

--- src/components/Moon.jsx
+++ src/components/Moon.jsx
@@ -9,43 +9,77 @@ class Moon extends React.Component {
         this.state = {
             error: null,
             data: null,
-            isLoaded: false,
+            isLoading: false,
+            timer: new Date(),
         };
     }
 
     componentDidMount() {
-        const currentYear = new Date().getFullYear();
-        const currentDay = new Date().getDate();
-        const currentMonth = new Date().getMonth() + 1;
-        const date = currentMonth + '/' + currentDay + '/' + currentYear;
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+
+        this.setState({ isLoading: true })
+        this.getData();
+
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            timer: new Date()
+        });
+    }
+
+    getData() {
+        const date = this.getDate();
 
         fetch(`http://api.usno.navy.mil/moon/phase?date=${date}&nump=2`)
             .then(res => res.json())
             .then(
                 (result) => {
-                    this.setState({
-                        isLoaded: true,
-                        data: result.phasedata,
-                    });
-                    console.log('Fetching Moon Phase Data - SUCCESS');
-                    console.log(this.state.data);
+                    if (result.error) {
+                        this.setState({
+                            error: result.type
+                        });
+                    } else {
+                        this.setState({
+                            data: result.phasedata,
+                            isLoading: false
+                        });
+                        console.log('Fetching Moon Phase Data - SUCCESS');
+                        console.log(this.state.data);
+                    }
                 },
 
                 (error) => {
                     this.setState({
-                        isLoaded: true,
-                        error
+                        error: error.message,
+                        isLoading: false
                     });
                 }
             )
     }
 
+    getDate() {
+        const currentYear = new Date().getFullYear();
+        const currentDay = new Date().getDate();
+        const currentMonth = new Date().getMonth() + 1;
+        const date = currentMonth + '/' + currentDay + '/' + currentYear;
+
+        return date;
+    }
+
     render() {
         if (this.state.error) {
             return (
-                <div>ERROR: {this.state.error.message}</div>
+                <div>ERROR: {this.state.error}</div>
             );
-        } else if (!this.state.isLoaded) {
+        } else if (this.state.isLoading) {
             return (
                 <div>Loading...</div>
             );
