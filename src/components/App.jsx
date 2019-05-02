ae8d6a7ee974b2734006193e5dbb8da5f917ff27
Caleb Perry <igadabif32@gmail.com>
2019-05-02 00:20:30 -0400

    Patching main App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,8 @@
 import React from "react"
 import Moon from "./Moon.jsx"
 import Wave from "./Wave.jsx"
-<<<<<<< HEAD
 import Meteors from "./Meteors.jsx"
-=======
 import Sky from "./Sky.jsx"
->>>>>>> d3a76ae63438ca52b0cb5e7f618cbf71e9fe0758
 
 //https://editor.p5js.org/projects/SkXAr20Jx TODO WEATHER
 class App extends React.Component {
@@ -17,12 +14,9 @@ class App extends React.Component {
     render() {
         return (
             <div>
-<<<<<<< HEAD
                 <Wave />
                 <Meteors />
-=======
                 <Sky />
->>>>>>> d3a76ae63438ca52b0cb5e7f618cbf71e9fe0758
                 <Moon />
                 <Wave />
             </div>
