240f1e3326cf555a184479c4b3f78500d0178b4f
Caleb Perry <igadabif32@gmail.com>
2019-05-02 00:55:41 -0400

    Un-blocking finished components from rendering.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,6 @@ class App extends React.Component {
         return (
             <div>
                 <Wave />
-                <Meteors />
                 <Sky />
                 <Moon />
                 <Wave />
